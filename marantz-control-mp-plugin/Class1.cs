8497e8cbb43f897394937ff321cbe31385c89ff2
mojo <mxb.mojo@gmail.com>
2019-10-27 14:33:33 +0100

    plugin skeleton

---
+++ marantz-control-mp-plugin/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediaPortal.GUI.Library;
+using MediaPortal.Dialogs;
+
+namespace marantz_control_mp_plugin
+{
+    public class Class1 : GUIWindow, ISetupForm
+    {
+        
+    #region ISetupForm Members
+
+    // Returns the name of the plugin which is shown in the plugin menu
+    public string PluginName()
+    {
+        return "MyFirstPlugin";
+    }
+
+    // Returns the description of the plugin is shown in the plugin menu
+    public string Description()
+    {
+        return "My First Plugin";
+    }
+
+    // Returns the author of the plugin which is shown in the plugin menu
+    public string Author()
+    {
+        return "YourNameHere";
+    }
+
+    // show the setup dialog
+    public void ShowPlugin()
+    {
+            MarantzSettings settings = new MarantzSettings();
+            settings.ShowDialog();
+            
+    }
+
+    // Indicates whether plugin can be enabled/disabled
+    public bool CanEnable()
+    {
+        return true;
+    }
+
+    // Get Windows-ID
+    public int GetWindowId()
+    {
+        // WindowID of windowplugin belonging to this setup
+        // enter your own unique code
+        return 9999;
+    }
+
+    // Indicates if plugin is enabled by default;
+    public bool DefaultEnabled()
+    {
+        return true;
+    }
+
+    // indicates if a plugin has it's own setup screen
+    public bool HasSetup()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// If the plugin should have it's own button on the main menu of MediaPortal then it
+    /// should return true to this method, otherwise if it should not be on home
+    /// it should return false
+    /// </summary>
+    /// <param name="strButtonText">text the button should have</param>
+    /// <param name="strButtonImage">image for the button, or empty for default</param>
+    /// <param name="strButtonImageFocus">image for the button, or empty for default</param>
+    /// <param name="strPictureImage">subpicture for the button or empty for none</param>
+    /// <returns>true : plugin needs it's own button on home
+    /// false : plugin does not need it's own button on home</returns>
+
+    public bool GetHome(out string strButtonText, out string strButtonImage,
+      out string strButtonImageFocus, out string strPictureImage)
+    {
+        strButtonText = String.Empty;
+        strButtonImage = String.Empty;
+        strButtonImageFocus = String.Empty;
+        strPictureImage = String.Empty;
+        return false;
+    }
+
+        // With GetID it will be an window-plugin / otherwise a process-plugin
+        // Enter the id number here again
+        //public override int GetID
+        //{
+        //    get
+        //    {
+        //        return 9991;
+        //    }
+
+        //    set
+        //    {
+        //    }
+        //}
+
+        #endregion
+
+        
+    }
+}
---
+++ marantz-control-mp-plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MediaPortal.Dialogs;
+using MediaPortal.Common.Utils;
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("marantz-control-mp-plugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("marantz-control-mp-plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a49f6eff-f7be-40df-8fac-d400c2271f5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CompatibleVersion("1.6.0.0")]
+[assembly: UsesSubsystem("MP.Input")]
+[assembly: UsesSubsystem("MP.Config")]
